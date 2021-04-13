74b90213488aec618acd240da1398c90037019fe
Brayan Lechon <balechon96@gmail.com>
2021-04-12 21:39:01 -0500

    primera version del juego

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-import re
-from unicodedata import normalize
+import random
+import os
 
 def read_data(): 
     with open("./data/data.txt",'r',encoding='utf-8') as df:
-        values=[word.rstrip('\n').replace('á','a') for word in df]
+        values=[word.rstrip('\n').upper() for word in df]
         values=removeAcent(values)
     return(values)
 
 def removeAcent(value):
     return [val.replace('á','a').replace('é','e').replace('í','i').replace('ó','o').replace('ú','u') for val in value]
 
+
+def startGame(data):
+    randomWord=random.choice(data)
+    print(randomWord)
+    word=[letter for letter in randomWord]
+    playerWord=['_' for _ in randomWord]
+
+    while word !=playerWord:
+        printList(playerWord)
+        choice=input('Escoge una letra: ')
+        for i in range(0,len(word)):
+            if word[int(i)]==choice:
+                playerWord[int(i)]= choice
+        os.system('clear')
+
+    print(f'*******GANASTE**** la palabra era {randomWord}')
+
+def printList(mylist):
+    for i in mylist:
+        print(i, end=" ")
+    print('\n')
+
 def run():
     df=read_data()
-    print(df)
+    startGame(df)
 
 if __name__=="__main__":
     run()
