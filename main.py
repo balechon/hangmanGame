3c95a96dfe20ee8e89652fd866c1c476c6877da7
Brayan Lechon <balechon96@gmail.com>
2021-04-12 23:49:40 -0500

    añadi ASCII art

--- main.py
+++ main.py
@@ -9,35 +9,94 @@ def read_data():
     return(values)
 
 def removeAcent(value):
-    return [val.replace('á','a').replace('é','e').replace('í','i').replace('ó','o').replace('ú','u') for val in value]
+    return [val.replace('Á','A').replace('É','E').replace('Í','I').replace('Ó','O').replace('Ú','U') for val in value]
 
 
-def startGame(data):
+def startGame(data, attemps):
     randomWord=random.choice(data)
     # print(randomWord)
     word=[letter for letter in randomWord]
     playerWord=['_' for _ in randomWord]
 
-    while word !=playerWord:
-        print('Adivina la Palabra')
+    while attemps != 0:
+        print("""
+           _    _  ____  _____    _____          _____   ____  
+     /\   | |  | |/ __ \|  __ \  / ____|   /\   |  __ \ / __ \ 
+    /  \  | |__| | |  | | |__) |  |       /  \  | |  | | |  | |
+   / /\ \ |  __  | |  | |  _  / | |      / /\ \ | |  | | |  | |
+  / ____ \| |  | | |__| | | \ \ | |____ / ____ \| |__| | |__| |
+ /_/    \_\_|  |_|\____/|_|  \_\ \_____/_/    \_\_____/ \____/ 
+                                                                                                 
+        ////////////////////////////////////////           
+        /#    *@                              ##                     
+        /#  *@,                               ##            
+        /#*@.                                 ##                       
+        /#                                 *@ @ @.           
+        /#                               .@*      /@.       
+        /#                               @.        .@       
+        /#                               @*        /&       
+        /#                                /@     /@/                                
+        /#                                   @@@@          
+        /#                                 @/ %@ \@        
+        /#                                @/  %@  \@                 
+        /#                                    %@           
+        /#                                   @  @          
+        /#                                  @    @          
+        /#                                 @      @              
+        /#                                  
+/#/#/#/#/#/#/#/#/#/#/#                                                                                   
+                                                              """)
+
+
+
+        print("\n")
+        print('Palabra:', end=" ")
         printList(playerWord)
         try:
+            print(f'Intentos Restantes: {attemps}')
             choice=input('Escoge una letra: ')
             if choice.isnumeric():
-                raise ValueError('Numbers are not allowed')
-                
-            for i in range(0,len(word)):
-                if word[int(i)]==choice.upper():
-                    playerWord[int(i)]= choice.upper()
+                raise ValueError('No se puede ingresar numeros')
+            if len(choice)>1:
+                raise ValueError('Solo una Letra o Vocal')
+
+            for idx, _ in enumerate(word):
+                if word[idx]==choice.upper():
+                    playerWord[idx]= choice.upper()
             os.system('clear')
+            attemps-=1
         except ValueError as ve:
             os.system('clear')
             print(f'  ****** Error: {ve} *******')
-            
-            time.sleep(2.5)
+            time.sleep(2)
             os.system('clear')
-    print(f'*******GANASTE**** la palabra era {randomWord}')
 
+    printList(playerWord)
+
+    newWord=input('Escribe la Palabra: ')
+    if newWord.upper()==randomWord:
+        print("""
+   _____          _   _           _____ _______ ______ 
+  / ____|   /\   | \ | |   /\    / ____|__   __|  ____|
+ | |  __   /  \  |  \| |  /  \  | (___    | |  | |__   
+ | | |_ | / /\ \ | . ` | / /\ \  \___ \   | |  |  __|  
+ | |__| |/ ____ \| |\  |/ ____ \ ____) |  | |  | |____ 
+  \_____/_/    \_\_| \_/_/    \_\_____/   |_|  |______|
+                                                                                                          
+        """)
+        print(f'La palabra era {randomWord}')
+    else:
+        print("""
+  ______      _      _                _____ _______ ______ 
+ |  ____/\   | |    | |        /\    / ____|__   __|  ____|
+ | |__ /  \  | |    | |       /  \  | (___    | |  | |__   
+ |  __/ /\ \ | |    | |      / /\ \  \___ \   | |  |  __|  
+ | | / ____ \| |____| |____ / ____ \ ____) |  | |  | |____ 
+ |_|/_/    \_\______|______/_/    \_\_____/   |_|  |______|
+                                                           
+        """)
+        print(f'La palabra era {randomWord}')
+    
 def printList(mylist):
     for i in mylist:
         print(i, end=" ")
@@ -45,7 +104,24 @@ def printList(mylist):
 
 def run():
     df=read_data()
-    startGame(df)
+    attemps= 8
+    while True:
+        startGame(df,attemps)
+        print("""
+ __      __   _                               _                      ___  
+ \ \    / /  | |                             | |                    |__ \ 
+  \ \  / /__ | |_   _____ _ __    __ _       | |_   _  __ _  __ _ _ __ ) |
+   \ \/ / _ \| \ \ / / _ \ '__|  / _` |  _   | | | | |/ _` |/ _` | '__/ / 
+    \  / (_) | |\ V /  __/ |    | (_| | | |__| | |_| | (_| | (_| | | |_|  
+     \/ \___/|_| \_/ \___|_|     \__,_|  \____/ \__,_|\__, |\__,_|_| (_)  
+                                                       __/ |              
+                                                      |___/   
+        """)
+        repeat=input("Si/No:  ")
+        if repeat.upper() =="NO":
+            break
+
+
 
 if __name__=="__main__":
     run()
